9813a04128dc24dc82bbb1a444e022bf00730632
silentfin <152957041+silentfin@users.noreply.github.com>
2025-09-03 23:46:13 +0530

    feat: finally all effects loop

---
+++ main.py
@@ -0,0 +1,45 @@
+import importlib
+
+all_effects = [
+    "ColorShift",
+    "Crumble",
+    "Decrypt",
+    "ErrorCorrect",
+    "Expand",
+    "Fireworks",
+    "Highlight",
+    "LaserEtch",
+    "Matrix",
+    "MiddleOut",
+    "OrbittingVolley",
+    "Overflow",
+    "Pour",
+    "Print",
+    "Rain",
+    "RandomSequence",
+    "Rings",
+    "Scattered",
+    "Slice",
+    "Slide",
+    "Spotlights",
+    "Spray",
+    "Swarm",
+    "Sweep",
+    "SynthGrid",
+    "Unstable",
+    "VHSTape",
+    "Waves",
+    "Wipe",
+]
+
+with open("logo.txt", "r") as file:
+    logo = file.read()
+
+for effect_name in all_effects:
+    module_path = f"terminaltexteffects.effects.effect_{effect_name.lower()}"
+    current_module = importlib.import_module(module_path)
+    effect_class = getattr(current_module, effect_name)
+    current_effect = effect_class(logo)
+    with current_effect.terminal_output() as terminal:
+        for frame in current_effect:
+            terminal.print(frame)
